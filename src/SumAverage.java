5e2a8e3db06e502bc1afb265afef62f07077c57a
Nataliya Romanyuk <nataromm1999@gmail.com>
2022-09-21 22:54:58 +0300

    My homework number 7!

--- src/SumAverage.java
+++ src/SumAverage.java
@@ -1,6 +1,5 @@
 public class SumAverage {
     public static int sum(int lowerBound, int upperBound) {
-        // TODO fill in code here using for loop and replace the return statement
         int temporary;
         int sum = 0;
         if (lowerBound > upperBound) {
@@ -12,12 +11,10 @@ public class SumAverage {
         for (int i = lowerBound; i <= upperBound; i++) {
             sum = sum + i;
         }
-
         return sum;
     }
 
     public static int sum(int[] numbers) {
-        // TODO fill in code here using for each loop and replace the return statement
         int sum = 0;
 
         for (int i = 0; i < numbers.length; i++) {
@@ -27,8 +24,6 @@ public class SumAverage {
     }
 
     public static double average(int lowerBound, int upperBound) {
-        // TODO fill in code here using while loop and replace the return statement
-
         int temporary;
         double sum = 0;
         int counter = 0;
@@ -42,12 +37,10 @@ public class SumAverage {
             sum = sum + i;
             counter++;
         }
-
         return sum / counter;
     }
 
     public static double average(int[] numbers) {
-
         double sum = 0;
         int counter = 0;
         for (int i = 0; i < numbers.length; i++) {
